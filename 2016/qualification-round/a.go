949730c67e5352e0ea5603846f60eecfe9757577
shiimaxx <yoshima@shiimaxx.com>
2017-04-07 22:53:25 +0900

    First commit

---
+++ 2016/qualification-round/a.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"math"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var list []string
+	var num int
+	fmt.Scanf("%d", &num)
+	fmt.Print(num)
+
+	sc := bufio.NewScanner(os.Stdin)
+
+	for i := 0; i < num; i++ {
+		sc.Scan()
+		j := sc.Text()
+		do {
+			a := j
+		} while
+	}
+}
